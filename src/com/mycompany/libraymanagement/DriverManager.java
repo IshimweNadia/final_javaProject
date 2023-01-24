dfdfc521285ec835647cd188df12bd7be5131ffa
IshimweNadia <118804982+IshimweNadia@users.noreply.github.com>
2023-01-24 18:18:43 +0200

    Add files via upload
    Adding libray management as final java project

---
+++ src/com/mycompany/libraymanagement/DriverManager.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.libraymanagement;
+
+import com.mysql.jdbc.Connection;
+
+/**
+ *
+ * @author Hp Folio 9480
+ */
+class DriverManager {
+
+    static Connection getConnection(String jdbcmysqllocalhost3306library, String root, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
